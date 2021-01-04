007f281e01b4230fde2dc370846d63c6197d1122
cwachira <cmuchir@gmail.com>
2021-01-04 12:19:23 +0300

    Updated the list of contributors

---
+++ e51a3977-6b2b99279a83d01dd291.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[25],{EubG:function(e,t,n){"use strict";n.d(t,"a",(function(){return i}));var r=n("z/Ev"),a=n("q1tI"),c=n.n(a),w=c.a.createElement("path",{d:"M30,28.59,22.45,21A11,11,0,1,0,21,22.45L28.59,30ZM5,14a9,9,0,1,1,9,9A9,9,0,0,1,5,14Z"}),i=c.a.forwardRef((function(e,t){var n=e.children,a=Object(r.b)(e,["children"]);return c.a.createElement(r.a,Object(r.c)({width:20,height:20,viewBox:"0 0 32 32",xmlns:"http://www.w3.org/2000/svg",fill:"currentColor",ref:t},a),w,n)}))}}]);
+//# sourceMappingURL=e51a3977-6b2b99279a83d01dd291.js.map
