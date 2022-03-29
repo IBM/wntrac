2ba33dfdd6f0deb17e083b1c6ac21fa11b4dcc8f
cwachira <cmuchir@gmail.com>
2022-03-29 13:38:38 +0300

    Updated the Data Browser link

--- component---src-pages-404-js-dbd40aa98779ba00c256.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{w2l6:function(e,n,t){"use strict";t.r(n);t("q1tI");var o=t("Wjw8"),s=t("qKvR"),r=[{href:"/components/markdown",text:"Markdown"},{href:"/components/Aside",text:"Aside"},{href:"/components/demo",text:"Demo"}];n.default=function(){return Object(s.b)(o.a,{links:r})}}}]);
-//# sourceMappingURL=component---src-pages-404-js-dbd40aa98779ba00c256.js.map
